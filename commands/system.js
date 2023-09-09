761be9b282f74cf28c222f7469a4f15ec752705b
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-09 22:20:42 +0100

    BLACK🥷

--- commands/system.js
+++ commands/system.js
@@ -343,12 +343,12 @@ cmd({
 *Hey, ${citel.pushName},*
 _This is  ${tlang().title}._
 ${alivemessage}
-
-*Version:-* _1.9.2_
-*Uptime:-* _${runtime(process.uptime())}_
-*Owner:-* _${Config.ownername}_
-*Branch:-* _${Config.BRANCH}_
-
+✧══════•❁❀❁•══════✧
+┨ *Version:-* _1.9.2_
+┨ *Uptime:-* _${runtime(process.uptime())}_
+┨ *Owner:-* _${Config.ownername}_
+┨ *Branch:-* _${Config.BRANCH}_
+✧══════•❁❀❁•══════✧
 _Type ${prefix}menu for my cmd list._
 
 _Powered by ${Config.ownername}_
