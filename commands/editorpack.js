d7c1ca5d7a366b278eae2cc78565db9d3f2204a9
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-11 00:53:15 +0100

    BLACK🥷

--- commands/editorpack.js
+++ commands/editorpack.js
@@ -37,42 +37,18 @@ CURRENTLY RUNNING ON BETA VERSION!!
    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    * SOFTWARE.
 **/
-const { smd,getBuffer, lang, botpic,photoEditor, prefix, Config } = require('../lib')
+const { smd, getBuffer, lang, botpic, photoEditor, prefix, Config } = require('../lib');
 
 const util = require('util');
 const fs = require('fs-extra');
-const axios = require('axios')
+const axios = require('axios');
 const fetch = require('node-fetch');
-const {TelegraPh} = require('../lib/scraper')
+const { TelegraPh } = require('../lib/scraper');
 
+let cap = Config.caption || "";
 
+smd({ pattern: "ad", category: "editor", filename: __filename, desc: "add view pic Editor." }, async (Suhail, msg, text, { cmdName }) => {
+    await photoEditor(Suhail, msg, cmdName, cap);
+});
 
-let cap =  Config.caption || "";
-
-
-smd({ pattern: "ad", category: "editor", filename: __filename,  desc: "add view pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "jail", category: "editor", filename: __filename,  desc: "jail pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "uncover", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "clown", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "mnm", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "pet", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "greyscale", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "invert", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "blur", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//============================================================================
-smd({ pattern: "drip", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "colorify", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "gun", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
-smd({ pattern: "wanted", category: "editor", filename: __filename,  desc: "pic Editor."},async(Suhail, msg, text , { cmdName}) => { await photoEditor(Suhail, msg , cmdName , cap); })
-//=============================================================================
+// Continue registering other photo editing commands in a similar way...
