7fcbd08c79ac630e9b5a33f3f34e24e1a1d59ce7
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-10 21:30:56 +0100

    BLACK🥷

--- commands/_updater.js
+++ commands/_updater.js
@@ -9,25 +9,33 @@
  * @version 0.0.6
  **/
 
-const DB = require('../lib/scraper')
-const { execSync } = require('child_process')
-const { tlang, Config, prefix,cmd } = require('../lib')
-    //---------------------------------------------------------------------------
-cmd({
-            pattern: "update",
-            desc: "Shows repo\'s refreshed commits.",
-            category: "misc",
-            filename: __filename
-        },
-        async(Void, citel, text,{ isCreator }) => {
-            if (!isCreator) return citel.reply('This command is only for my owner')
-            let commits = await DB.syncgit()
-            if (commits.total === 0) { citel.reply(`Hey ${citel.pushName}. You have latest version installed.`)} 
-            else {
-                let update = await DB.sync()
-                return await Void.sendMessage(citel.chat, { text: update,});
-            }
+const DB = require('../lib/scraper');
+const { execSync } = require('child_process');
+const { tlang, Config, prefix, cmd } = require('../lib');
 
+cmd({
+    pattern: "update",
+    desc: "Shows repo's refreshed commits.",
+    category: "misc",
+    filename: __filename
+},
+async (Void, citel, text, { isCreator }) => {
+    if (!isCreator) return citel.reply('This command is only for my owner');
+
+    try {
+        citel.reply("Fetching updates... Please wait.");
+
+        let commits = await DB.syncgit();
+        if (commits.total === 0) {
+            citel.reply(`Hey ${citel.pushName}. You have the latest version installed.`);
+        } else {
+            citel.reply("Updating... This may take a moment.");
+            let update = await DB.sync();
+            citel.reply("Update completed successfully.");
         }
-    )
-  
+    } catch (error) {
+        console.error("Update error:", error);
+        citel.reply("An error occurred during the update. Please try again later.");
+    }
+});
+
